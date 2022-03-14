a13b0da19088bf770aa0fe0a8ee894b68385f7e3
Nitishnaidu368 <82079830+Nitishnaidu368@users.noreply.github.com>
2022-03-14 09:47:11 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,22 @@
+var web3;
+var address ="";
+
+async function connect() {
+    await window.web3.currentprovider.enable();
+    web3=new Web3(window.web3.currentprovider);
+}
+
+if(typeof web3 !== "undefined"){
+    web3=new Web3(window.web3.currentprovider);
+}
+else {
+    web3=new Web3(window.web3.provider.HttpProvider("HTTP://127.0.0.1:7545"));
+}
+
+var abi = [];
+
+var contract =new web3.eth.Contract(abi,address);
+
+function transfer(){
+    
+}
